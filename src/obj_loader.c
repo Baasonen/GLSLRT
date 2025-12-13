7b5316cda5522a0789f8113b36fc8595d570279e
Baasonen <hejupa035@gmail.com>
2025-12-13 02:02:16 +0200

    obj progress

--- src/obj_loader.c
+++ src/obj_loader.c
@@ -13,3 +13,62 @@ typedef struct
     size_t capacity;
 } TempVertexList;
 
+typedef struct 
+{
+    unsigned int* data;
+    size_t size;
+    size_t capacity;
+} TempIndexList;
+
+void init_vertex_list(TempVertexList* list)
+{
+    list->size = 0;
+    list->capacity = 10;
+    list->data = (TempVertex*)malloc(list->capacity * sizeof(TempVertex));
+}
+
+void push_vertex(TempVertexList* list, float x, float y, float z)
+{
+    if (list->size >= list->capacity)
+    {
+        list->capacity *= 2;
+        list->data = (TempVertex*)realloc(list->data, list->capacity * sizeof(TempVertex));
+    }
+    list->data[list->size++] = (TempVertex){x, y, z};
+}
+
+void init_index_list(TempIndexList* list)
+{
+    list->size = 0;
+    list->capacity = 30; // 10 Tris
+    list->data = (unsigned int*)malloc(list->capacity * sizeof(unsigned int));
+}
+
+void push_index(TempIndexList* list, unsigned int index)
+{
+    if (list->size >= list->capacity)
+    {
+        list->capacity *= 2;
+        list->data = (unsigned int*)realloc(list->data, list->capacity * sizeof(unsigned int));
+    }
+    list->data[list->size++] = index;
+}
+
+// Main loader
+MeshData load_obj(const char* filename)
+{
+    FILE* file = fopen(filename, "r");
+    if (!file)
+    {
+        fprintf(stderr, "Could not open file %s\n", filename);
+        return (MeshData){NULL, NULL, 0, 0};
+    }
+
+    TempVertexList temp_positions;
+    TempIndexList temp_indices;
+    init_vertex_list(&temp_positions);
+    init_index_list(&temp_indices);
+
+    char line_buffer[128];
+    MeshData final_mesh = {NULL, NULL, 0, 0};
+}
